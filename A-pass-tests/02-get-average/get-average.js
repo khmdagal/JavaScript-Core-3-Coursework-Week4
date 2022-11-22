ecd2ed7b00041dbc8c9a6c182b20fd308e0f582e
khmdagal <khmdagal@gmail.com>
2022-11-22 23:40:53 +0000

    Get-average test passed

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,23 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
-
+function average(numbers) {
+    const filterd = []
+    for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] === 'number') {
+        filterd.push(numbers[i])
+    }
+        
+    }
+    
+    return filterd.reduce((a,b)=> (a+b))/filterd.length
+}
+console.log(average([4, "-", 8, 11, "hello", "57", 0, 2]));
 module.exports = average;
+
+/*
+ numbers.filter((element) => {
+      filterd.push(typeof element === "number");
+     });
+    console.log(filterd)
+*/
