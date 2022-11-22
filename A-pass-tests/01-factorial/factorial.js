ba36521a4be6c3ef26185e92abd5e9b7e219642f
khmdagal <khmdagal@gmail.com>
2022-11-22 23:06:24 +0000

    A-pass-test factorial

--- A-pass-tests/01-factorial/factorial.js
+++ A-pass-tests/01-factorial/factorial.js
@@ -8,6 +8,19 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+    let iteration = []
+    for (let i = 1; i <= int; i++){
+        iteration.push(i);
+        
+    }
+
+    return iteration.reduce((a, b) => {
+        return a * b;
+    });
+}
+
+
+
 
 module.exports = factorial;
