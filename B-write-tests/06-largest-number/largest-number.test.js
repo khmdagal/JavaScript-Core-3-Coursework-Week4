0ea582fea73902d147531877bcf699233e6a80c6
khmdagal <khmdagal@gmail.com>
2022-11-27 18:50:02 +0000

    Large munber test passed

--- B-write-tests/06-largest-number/largest-number.test.js
+++ B-write-tests/06-largest-number/largest-number.test.js
@@ -2,8 +2,14 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  const input = [3, 21, 88, 4, 36];
+const output = getLargestNumber(input);
   // Act
+  const sorted = input.sort((a, b) => a - b);
+  const lastIndex = sorted[sorted.length - 1];
   // Assert
+  
+  expect(lastIndex).toEqual(output);
 });
 
 // example
